6b5be784c6fd13af61ee2e1f4b9f85ab2e7764bb
ragul <ragulcdm@gmail.com>
2021-06-08 12:20:51 +0530

    new changes

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const LocalStrategy = require('passport-local');
 const flash=require('connect-flash');
 const session = require('express-session');
 const { countReset } = require('console');
+const reader=require('xlsx');
 const app=express();
 const firebase=require('firebase');
 const multer=require('multer');
+const { stringify } = require('querystring');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads/');
@@ -19,8 +21,7 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage: storage });
-//const fileUpload=require('express-fileupload');
-//const { nextTick } = require('node:process');
+
 var firebaseConfig = {
     apiKey: "AIzaSyCu1W_ewdzxHt9E_rcmGRDB9CFSbIdryYg",
     authDomain: "se-project-135ac.firebaseapp.com",
@@ -80,7 +81,11 @@ const CourseSchema=new mongoose.Schema({
         unique:true,
         required:true,
     },
-    code: String,
+    courseCode: {
+        type:String,
+        unique:true,
+        required:true,
+    },
     fileNot:{
         type:Number,
         default:2
@@ -95,12 +100,33 @@ const CourseSchema=new mongoose.Schema({
     }
 })
 
+const MarkfileSchema=new mongoose.Schema({
+    courseCode: String,
+    UploadedBy:{
+        id: {type:mongoose.Schema.Types.ObjectId,ref:'User'},
+        name: String,
+    },
+    fileData:{
+        fieldname: String,
+        originalname: String,
+        encoding: String,
+        mimetype: String,
+        destination: String,
+        filename: String,
+        path: String,
+        size: Number
+    },
+    UploadedDate: String,
+    markList: []
+})
+
 ///////////////////////////////////////////////////////
 
 UserSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User',UserSchema);
 const File=mongoose.model('File',FileSchema);
 const Course=mongoose.model('Course',CourseSchema);
+const Markfile=mongoose.model('Markfile',MarkfileSchema);
 
 app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -134,10 +160,14 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.get('/',(req,res)=>{
-    res.render('register.ejs');
+    res.redirect('/login');
 })
 
-app.post('/register',(req,res)=>{
+app.get('/admin/register',(req,res)=>{
+    res.render('adminRegister.ejs');
+})
+
+app.post('/admin/register',(req,res)=>{
     const {username,password,name,phoneNumber,userType}=req.body;
     auth.createUserWithEmailAndPassword(username,password)
     .then((user)=>{
@@ -148,7 +178,7 @@ app.post('/register',(req,res)=>{
                 console.log(err);
             }
             else{
-                res.redirect('/home');
+                res.redirect('/admin/register');
             }
         })
 
@@ -163,9 +193,76 @@ app.post('/register',(req,res)=>{
         res.redirect('/');
         console.log(e);
     })
+})
+
+app.get('/admin/course',(req,res)=>{
+    Course.find({},(err,courses)=>{
+        res.render('courseRegister.ejs',{courses:courses});
+    })
+    
+})
+
+app.post('/admin/course',(req,res)=>{
+    const {courseName,courseCode}=req.body;
+    const newCourse= new Course({courseName:courseName.toUpperCase(),courseCode:courseCode.toUpperCase()});
+    newCourse.save((err,savedCourse)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect('/admin/course');
+        }
+    })
+})
+
+app.get('/admin/course/delete/:course_id',(req,res)=>{
+    Course.findByIdAndDelete(req.params.course_id,(err,courses)=>{
+        if(err){
+            console.log(err);
+        }
+        res.redirect('/admin/course');
+    })
+})
+
+app.get('/admin/user',(req,res)=>{
+    User.find({},(err,user)=>{
+        if(!err){
+            res.render('adminUser.ejs',{userList:user});
+        }
+    })
+    
+})
+
+//both are common for students and teachers
+app.get('/admin/user/update/:user_id',(req,res)=>{
+    Course.find({},(err1,courses)=>{
+        User.findById(req.params.user_id,(err,user)=>{
+            res.render('userUpdate.ejs',{user:user,courses:courses});
+        })
+    })
 
 })
 
+app.post('/admin/user/edit/:user_id',(req,res)=>{
+    if(req.body.newcourseList && req.body.newcourseList.length){
+        if(typeof(req.body.newcourseList)=='string'){
+            User.findById(req.params.user_id,(err,user)=>{
+                user.courseList=[];
+                user.courseList.push(req.body.newcourseList);
+                user.save();
+            })
+        } else{
+            User.findById(req.params.user_id,(err,user)=>{
+                user.courseList=[];
+                req.body.newcourseList.forEach(function(id){
+                    user.courseList.push(id);
+                })
+                user.save();
+            })
+        }
+    }
+    res.redirect('/admin/user/update/'+req.params.user_id);
+})
 
 app.get('/login',(req,res)=>{
     res.render('login.ejs');
@@ -248,9 +345,6 @@ app.get('/courses/dashboard/:course_id',(req,res)=>{
     })
 });
 
-app.get('/courseRegister',(req,res)=>{
-    res.render("courseRegister.ejs");
-})
 
 app.post('/courseRegister',(req,res)=>{
     const {courseName}=req.body;
@@ -344,7 +438,7 @@ app.post('/update/profile/:fac_id',(req,res)=>{
 });
 
 app.get('/:course_id/file/upload',(req,res)=>{
-    File.find({}).populate('UploadedBy').exec((err,files)=>{
+    File.find({courseID:req.params.course_id}).populate('UploadedBy').exec((err,files)=>{
         res.render('filePage.ejs',{course_id:req.params.course_id,files:files,currentUser:req.session.currentUser});
     });
 });
@@ -354,24 +448,16 @@ app.post('/:course_id/file/upload',upload.single('newfile'),(req,res)=>{
     const date=new Date();
     const today=date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
     const newFile=new File({courseID:mongoose.Types.ObjectId(req.params.course_id),fileData:newfile,
-                            UploadedBy:mongoose.Types.ObjectId(req.session.currentUser._id),UploadedDate:today});
-    newFile.save((err1,savedFile)=>{
-        if(err1){
-            console.log(err1);
-        }
-    })
-    /*Course.findById(req.params.course_id,(err,course)=>{
-        const newFile=new File({courseID:course,fileData:newfile});
+        UploadedBy:mongoose.Types.ObjectId(req.session.currentUser._id),UploadedDate:today});
         newFile.save((err1,savedFile)=>{
             if(err1){
-                console.log(err1);
+            console.log(err1);
             }
         })
-    });*/
-    res.redirect('/'+req.params.course_id+'/file/upload');
-    
+        res.redirect('/'+req.params.course_id+'/file/upload');    
 });
 
+//common for all files
 app.get('/:file_id/file/download',(req,res)=>{
     File.findById(req.params.file_id,(err,foundFile)=>{
         res.download('uploads/'+foundFile.fileData.filename,foundFile.fileData.filename.split("-")[0]);
@@ -383,6 +469,73 @@ app.get('/:file_id/:course_id/file/delete',async (req,res)=>{
     await File.findByIdAndDelete(req.params.file_id);
     res.redirect('/'+req.params.course_id+'/file/upload');
 })
+
+// mark files
+app.get('/:course_id/markfile/upload',(req,res)=>{
+    if(req.session.currentUser.userType=='Teacher'){
+        Markfile.find({}).exec((err,files)=>{
+            res.render('markfilePage.ejs',{course_id:req.params.course_id,files:files,currentUser:req.session.currentUser});
+        });
+    }else{
+        Course.findById(req.params.course_id,(err,foundcourse)=>{
+            Markfile.findOne({courseCode:foundcourse.courseCode},(err,markfile)=>{
+                if(markfile){
+                    var markobj=markfile['markList'].find((ele)=>{
+                        return ele['RollNumber']==req.session.currentUser.username.split("@")[0].toUpperCase();
+                    });
+                    console.log(markobj);
+                    res.render('studentMarkpage.ejs',{markobj:markobj});
+                }
+                else{
+                    res.render('studentMarkpage.ejs',{markobj:markfile});
+                }
+            })
+        })
+    }
+})
+
+app.post('/:course_id/markfile/upload',upload.single('newfile'),(req,res)=>{
+    const newfile=req.file;
+    const date=new Date();
+    console.log(req.body);
+    const today=date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
+    Course.findById(req.params.course_id,(err,foundcourse)=>{
+        if(!err){
+            User.findById(req.session.currentUser._id,(err1,founduser)=>{
+                const file=reader.readFile('uploads/'+newfile.filename);
+                let data=[];
+                const temp = reader.utils.sheet_to_json(
+                    file.Sheets[file.SheetNames[0]])
+                temp.forEach((res) => {
+                    data.push(res);
+                })
+                console.log(data);
+                console.log(foundcourse);
+                const newFile=new Markfile({courseCode:foundcourse.courseCode,fileData:newfile,
+                    UploadedBy:{id:mongoose.Types.ObjectId(req.session.currentUser._id),name:founduser.name},UploadedDate:today,markList:data});
+                newFile.save((err2,savedFile)=>{
+                    if(err2){
+                        console.log(err2);
+                    }
+                })
+            })
+        }
+    })
+    res.redirect('/'+req.params.course_id+'/markfile/upload');
+    
+});
+
+app.get('/:file_id/:course_id/markfile/delete',(req,res)=>{
+    Markfile.findByIdAndDelete(req.params.file_id,(err,foundfile)=>{
+        if(!err){
+            res.redirect('/'+req.params.course_id+'/markfile/upload');
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
+
 
 app.get('/faculty/:fac_id',(req,res)=> {
         var faculty;
@@ -399,9 +552,6 @@ app.get('/faculty/:fac_id',(req,res)=> {
         });
 })
 
-app.get('/courseAssign',(req,res)=>{
-    res.render('courseAssign.ejs');
-})
 
 app.listen(4000, () => {
     console.log('Serving on port 4000')
